269de2d95e2bcb9132f74af3a683dfa7de244568
Abdi Negara Guci <abdinegaraguci@Abdis-MacBook-Air.local>
2025-05-04 21:31:06 +0700

    add a deployment Files

--- apps/settings.py
+++ apps/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vp6s*uppz$zz-i03#@8ze-x_&by)f!&v2+0#f-enwuc37pye!*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
